4ba40c6105d7649d49d780fdce38491ab2accc06
“josh” <joshrhodes60@icloud.com>
2021-08-30 16:23:51 -0500

    starting progress on delete button

--- src/main/java/org/launchcode/inventorymanagement/JavaWebDevTechjobsAuthenticationApplication.java
+++ src/main/java/org/launchcode/inventorymanagement/JavaWebDevTechjobsAuthenticationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class inventoryManagement {
+public class JavaWebDevTechjobsAuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(inventoryManagement.class, args);
+		SpringApplication.run(JavaWebDevTechjobsAuthenticationApplication.class, args);
 	}
 
 }
